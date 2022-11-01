bcdd6716add217d858c05b92efeb0f1e17fe8894
laynejohnson13 <laynejohnson13001@gmail.com>
2022-11-01 13:56:24 -0400

    app.py edited

--- CRUD assignment/app.py
+++ CRUD assignment/app.py
@@ -49,5 +49,128 @@ class Patients(db.Model):
         }
 
 
+
+class Conditions_patient(db.Model):
+    __tablename__ = 'patient_conditions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    mrn = db.Column(db.String(255), db.ForeignKey('production_patients.mrn'))
+    icd10_code = db.Column(db.String(255), db.ForeignKey('conditions.icd10_code')) ###make sure this line is correct
+
+    # this first function __init__ is to establish the class for python GUI
+    def __init__(self, mrn, icd10_code):
+        self.mrn = mrn
+        self.icd10_code = icd10_code
+
+    # this second function is for the API endpoints to return JSON
+    def to_json(self):
+        return {
+            'id': self.id,
+            'mrn': self.mrn,
+            'icd10_code': self.icd10_code
+        }
+
+class Conditions(db.Model):
+    __tablename__ = 'conditions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    icd10_code = db.Column(db.String(255))
+    icd10_description = db.Column(db.String(255))
+
+    # this first function __init__ is to establish the class for python GUI
+    def __init__(self, icd10_code, icd10_description):
+        self.icd10_code = icd10_code
+        self.icd10_description = icd10_description
+
+    # this second function is for the API endpoints to return JSON
+    def to_json(self):
+        return {
+            'id': self.id,
+            'icd10_code': self.icd10_code,
+            'icd10_description': self.icd10_description
+        }
+
+
+
+
+class Medications_patient(db.Model):
+    __tablename__ = 'patient_medications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    mrn = db.Column(db.String(255), db.ForeignKey('production_patients.mrn'))
+    med_ndc = db.Column(db.String(255), db.ForeignKey('medications.med_ndc'))
+
+    # this first function __init__ is to establish the class for python GUI
+    def __init__(self, mrn, med_ndc):
+        self.mrn = mrn
+        self.med_ndc = med_ndc
+
+    # this second function is for the API endpoints to return JSON
+    def to_json(self):
+        return {
+            'id': self.id,
+            'mrn': self.mrn,
+            'med_ndc': self.med_ndc
+        }
+    
+class Medications(db.Model):
+    __tablename__ = 'medications'
+
+    id = db.Column(db.Integer, primary_key=True)
+    med_ndc = db.Column(db.String(255))
+    med_human_name = db.Column(db.String(255))
+
+    # this first function __init__ is to establish the class for python GUI
+    def __init__(self, med_ndc, med_human_name):
+        self.med_ndc = med_ndc
+        self.med_human_name = med_human_name
+
+    # this second function is for the API endpoints to return JSON
+    def to_json(self):
+        return {
+            'id': self.id,
+            'med_ndc': self.med_ndc,
+            'med_human_name': self.med_human_name
+        }
+
+
+
+#### BASIC ROUTES WITHOUT DATA PULSL FOR NOW ####
+@app.route('/')
+def index():
+    return render_template('landing.html') ##change templates
+
+@app.route('/signin')
+def signin():
+    return render_template('/signin.html') ##change templates
+
+
+
+
+##### CREATE BASIC GUI FOR CRUD #####
+@app.route('/patients', methods=['GET'])
+def get_gui_patients():
+    returned_Patients = Patients.query.all() # documentation for .query exists: https://docs.sqlalchemy.org/en/14/orm/query.html
+    return render_template("patient_all.html", patients = returned_Patients)
+
+# this endpoint is for inserting in a new patient
+@app.route('/insert', methods = ['POST'])
+def insert(): # note this function needs to match name in html form action 
+    if request.method == 'POST':
+        mrn = request.form['mrn']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        gender = request.form['gender']
+        zip_code = request.form['zip_code']
+        new_patient = Patients(mrn, first_name, last_name, gender, zip_code)
+        db.session.add(new_patient)
+        db.session.commit()
+        flash("Patient Inserted Successfully")
+        return redirect(url_for('get_gui_patients'))
+    else:
+        flash("Something went wrong")
+        return redirect(url_for('get_gui_patients'))
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
